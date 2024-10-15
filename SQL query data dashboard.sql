208834c0cc44fc36c9b3d477d5d4646ac49218ba
Simran Faizi <110330330+Simranfaizi@users.noreply.github.com>
2024-10-15 13:58:45 +0530

    Add files via upload

---
+++ SQL query data dashboard.sql
@@ -0,0 +1,23 @@
+select * from credit_card;
+
+
+
+BULK INSERT dbo.credit_card
+FROM 'C:\\Users\\Simran\\OneDrive\\Desktop\\powerbidatasets\\credit card analysis\\cc_add.csv'
+WITH (
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2,  -- Skip the header
+    TABLOCK
+);
+
+BULK INSERT dbo.customer
+FROM 'C:\\Users\\Simran\\OneDrive\\Desktop\\powerbidatasets\\credit card analysis\\cust_add.csv'
+WITH (
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2,  -- Skip the header
+    TABLOCK
+);
+
+select * from customer;
